44e5225794f787c0d801062d4d904610875b28d4
shoebby <alexandria.vaneekelen@gmail.com>
2025-01-25 22:40:41 +0100

    again a lot
    - todo page for about section
    - first iteration of blog page and setup for the first blog to be written
    - bunch of buttons
    - bunch of .kra workfiles
    - renamed main.html to index.html for nekoweb
    - sound!
    - more work on the main page

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 //how tf do i not make it need to do this bru
 window.onload = () => {
-    //pfp grow on click
+    //pfp arf and shake on click
     var pfp = document.getElementById("pfp");
-    var pfpSize = 125;
+    var pfpSize = 200;
+    
     pfp.onclick = function () {
-        pfpSize += 10;
+        pfpSize -= 10;
         pfp.style.width = pfpSize + "px";
-        
+
+        var sound_oof = new Audio("sounds/oof.mp3");
+        sound_oof.play();
     };
 
     //image spin on click
@@ -28,5 +31,28 @@ window.onload = () => {
     // document.getElementById("welcomeBox").style.color = "blue";
 };
 
+//--------------------------------jana containment field-----------------------------------
+// function fib(n) {
+//     if (n == 0) throw "penis";
+//     if (n == 1) return 1;
+//     return fib(n - 1) + fib(n - 2);
+// }
 
+// function checkorder(arr) {
+//     if (length(arr) == 0) throw "cock";
+//     for (var i = 1; i < length(arr); i++) {
+//         if (arr[i - 1] > arr[i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
+// function sleepsort(arr) {
+//     var out = [];
+//     var sleepers = [];
+//     for (var i = 0; i < arr.length; i++) {
+
+//         setTimeout(() => {out.push(i)}, i);
+//     }
+// }
