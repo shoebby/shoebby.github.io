7441998f204ae9d81a85019f058e8f5b16f93f62
shoebby <alexandria.vaneekelen@gmail.com>
2025-01-27 17:31:51 +0100

    oh goodododdoddddd
    - cursor?
    - favicon and other meta head stuff
    - changed navbar in blog index
    - added a last.fm last scrobble widget to the main page
    - added a lil player for background music (want to improve to actual good player)

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
+import "./node_modules/jquery/dist/jquery.js";
+
 //how tf do i not make it need to do this bru
 window.onload = () => {
+    //background music
+    var bgm = new Audio("sounds/strawberriesandlancables.mp3");
+    var bgmText = document.getElementById("bgmText");
+    var playbutton = document.getElementById("playbgm");
+
+    bgm.loop = true;
+    
+    playbutton.onclick = function () {
+        if (isPlaying(bgm)) {
+            bgmText.innerHTML = "< 'click me to play some tunes!'";
+            bgm.pause();
+            return;
+        }
+        bgmText.innerHTML = "< 'click me to pause these tunes!'";
+        bgm.play();
+    }
+
+    function isPlaying(audioEl) {
+        return !audioEl.paused;
+    }
+
+    //checking last.fm for most recent track
+    var link = document.getElementById("trackLink");
+    var cover = document.getElementById("trackCover");
+    var title = document.getElementById("trackName");
+    var artist = document.getElementById("trackArtist");
+
+    function CurrentlyPlaying() {
+        $.get(`http://ws.audioscrobbler.com/2.0/`,
+            {
+                "method": "user.getrecenttracks",
+                "limit": "1",
+                "user": "shoebby",
+                "api_key": API_KEY,
+                "format": "json"
+            }, (data, status) => {
+                console.log(data);
+                console.log(status);
+
+                cover.src = data.recenttracks.track[0].image[1]["#text"];
+                link.href = data.recenttracks.track[0].url;
+                title.innerHTML = data.recenttracks.track[0].name;
+                artist.innerHTML = data.recenttracks.track[0].artist["#text"];
+            }
+        )
+    };
+    CurrentlyPlaying();
+
     //pfp arf and shake on click
     var pfp = document.getElementById("pfp");
     var pfpSize = 200;
