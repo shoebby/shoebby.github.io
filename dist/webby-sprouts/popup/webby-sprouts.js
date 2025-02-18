776ecb1754af343470ad171af9991ed3332c3bfd
shoebby <alexandria.vaneekelen@gmail.com>
2025-02-18 01:22:47 +0100

    more web extension stuff
    works more like it should now waow

--- dist/webby-sprouts/popup/webby-sprouts.js
+++ dist/webby-sprouts/popup/webby-sprouts.js
@@ -13,8 +13,6 @@
 //var depth = input_depth.value;
 //var treeNum = input_treeNum.value;
 
-
-
 // input_startLength.oninput = function () {
 //     startLength = this.value;
 // }
@@ -34,38 +32,61 @@
 //     treeNum = this.value;
 // }
 
-const hidePage =    `body > :not(.beastify-image) {
-                        display: none;
-                    }`;
+const branchCSS =   `.branchDiv { 
+                        position: absolute;
+                        display: flex;
+                        width: var(--w);
+                        height: 3px;
+                        left: 100%;
+                        transform-origin: 0% 50%;
+                        transform: rotate(var(--a));
+                        background-color: rgba(var(--colorR), var(--colorG), var(--colorB), var(--colorA));
+                        color: rgba(0, 0, 0, 0);
+                        font-size: 10px;
+
+                        animation-name: branchFlow;
+                        animation-duration: 0s;
+                        animation-iteration-count: infinite;
+                        animation-timing-function: ease-in-out;
+                    }
+                    .rootDiv {
+                        left: 50%;
+                        top: 0%;
+                        transform-origin: 0% 50%;
+                    }
+                    .branchDiv:hover {
+                        color: white;
+                        background-color: orange;
+                    }
+                    @keyframes branchFlow {
+                        0% {
+                            height: 1px;
+                            rotate: -1deg;
+                        }
+                        50% {
+                            height: 4px;
+                            rotate: 1deg;
+                        }
+                        100% {
+                            height: 1px;
+                            rotate: -1deg;
+                        }
+                    }
+                `;
 
 function listenForClicks() {
     document.addEventListener("click", (e) => {
-        /**
-         * Given the name of a beast, get the URL to the corresponding image.
-         */
-        function beastNameToURL(beastName) {
-            switch (beastName) {
-                case "Soft":
-                    return browser.runtime.getURL("images/soft.PNG");
-            }
-        }
 
-        function beastify(tabs) {
-            const url = beastNameToURL(e.target.textContent);
-            browser.tabs.sendMessage(tabs[0].id, {
-                command: "beastify",
-                beastURL: url,
-            });
+        function sproutify(tabs) {
+            browser.tabs.insertCSS({ code: branchCSS }).then(() => browser.tabs.sendMessage(tabs[0].id, {command: "sproutify",}));
         }
 
         function reset(tabs) {
-            browser.tabs.sendMessage(tabs[0].id, {
-                command: "reset",
-            });
+            browser.tabs.sendMessage(tabs[0].id, {command: "reset",});
         }
 
         function reportError(error) {
-            console.error(`Could not beastify: ${error}`);
+            console.error(`Could not sproutify: ${error}`);
         }
 
         if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
@@ -81,7 +102,7 @@ function listenForClicks() {
         } else {
             browser.tabs
                 .query({ active: true, currentWindow: true })
-                .then(beastify)
+                .then(sproutify)
                 .catch(reportError);
         }
     });
@@ -90,13 +111,10 @@ function listenForClicks() {
 function reportExecuteScriptError(error) {
     document.querySelector("#popup-content").classList.add("hidden");
     document.querySelector("#error-content").classList.remove("hidden");
-    console.error(`Failed to execute beastify content script: ${error.message}`);
+    console.error(`Failed to execute webby sprout content script: ${error.message}`);
 }
 
 browser.tabs
-    .executeScript({ file: "/content_scripts/beastify.js" })
+    .executeScript({ file: "/content_scripts/sproutify.js" })
     .then(listenForClicks)
     .catch(reportExecuteScriptError);
-
-// ------------------------------------------------------------------------------------------------------------------------- //
-
