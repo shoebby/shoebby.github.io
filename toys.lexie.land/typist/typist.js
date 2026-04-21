1fae1ffc48328d956db713324b2d2f941ad4b913
Alexandria <alexandria.vaneekelen@gmail.com>
2026-04-21 16:23:57 +0200

    DEMON

--- toys.lexie.land/typist/typist.js
+++ toys.lexie.land/typist/typist.js
@@ -1,7 +1,6 @@
 import stringifyStylesheet from '../libraries/stringify-css-rule.js'
 
 const writingText = document.querySelector("#text");
-let storedText = writingText.innerHTML;
 let replaceAll = false;
 
 const typingSound = new Howl({
@@ -19,6 +18,21 @@ const buttonSounds = new Howl({
         glow:   [3354,  796],
         tilt:   [4150,  825]
     }
+});
+
+const fireSound = new Howl({
+    src: ['../divbrush/assets/audio/flames.mp3']
+});
+
+const paperSound_reveal = new Howl({
+    src: ['./assets/paper-reveal.mp3']
+});
+const paperSound_crush = new Howl({
+    src: ['./assets/paper-crush.mp3']
+});
+
+const laughSound = new Howl({
+    src: ['./assets/laugh.mp3']
 });
 
 const animStyle = document.createElement("style");
@@ -47,6 +61,77 @@ function ApplyEffect(styleString, sound) {
 
     buttonSounds.play(sound)
 }
+
+const bgparent = document.querySelector("#background");
+let pointsArray_circle = [[250, 0], [-250, 0], [0, -250], [0, 250], [175, 175], [-175, 175], [175, -175], [-175, -175]];
+
+let pointsArray_startip_1 = [[175, 75], [0, -50], [-175, 75]];
+let pointsArray_startip_2 = [[0, 200], [125, -150], [-75, 0]];
+let pointsArray_startip_3 = [[0, 200], [-125, -150], [75, 0]];
+let colorStep;
+let sizeStep;
+let startSize = 4;
+
+function initbg(depth) {
+    colorStep = 50 / depth;
+    sizeStep = startSize / depth;
+
+    const startPoint_circle = document.createElement("div");
+    startPoint_circle.classList = "point spins";
+    startPoint_circle.style.left = "50vw";
+    startPoint_circle.style.top = "45vh";
+    setPointStyle(startPoint_circle, depth + 1);
+    bgparent.appendChild(startPoint_circle);
+
+    const startPoint_star = document.createElement("div");
+    startPoint_star.classList.add("point");
+    startPoint_star.style.left = "50vw";
+    startPoint_star.style.top = "45vh";
+    setPointStyle(startPoint_star, depth + 1);
+    bgparent.appendChild(startPoint_star);
+
+    //circle
+    buildbg(pointsArray_circle, depth, startPoint_circle, 0.7);
+
+    //star
+    buildbg(pointsArray_startip_1, 5, startPoint_star, 0.7);
+    buildbg(pointsArray_startip_2, 5, startPoint_star, 0.7);
+    buildbg(pointsArray_startip_3, 5, startPoint_star, 0.7);
+}
+
+function buildbg(points, depth, parent, ratio) {
+    if (depth == 0) {
+        return;
+    }
+
+    for (var i = 0; i < points.length; i++) {
+        const newPixel = document.createElement("div");
+        
+        parent.appendChild(newPixel);
+        if (parent.classList == "point spins")
+            newPixel.classList = "point spins";
+        else
+            newPixel.classList = "point";
+        
+        newPixel.style.transform += `translateX(${ points[i][0] }px) translateY(${ points[i][1] }px)`;
+
+        setPointStyle(newPixel, depth);
+
+        var newPoints = points.map((point) => point.map((coord) => coord * ratio));
+
+        buildbg(newPoints, depth - 1, newPixel, ratio);
+    }
+}
+function setPointStyle(point, pointDepth) {
+    point.style.backgroundColor = `hsl(${(((colorStep * pointDepth) * 2) - 80)}deg, 100%, ${(colorStep * pointDepth) + 10}%)`;
+    point.style.boxShadow = `
+        0 0 10px 3px
+        hsl(${(((colorStep * pointDepth) * 2) - 80)}deg, 100%, ${(colorStep * pointDepth) + 10}%)
+        `;
+    point.style.width = `${(sizeStep * pointDepth) + 2}px`;
+    point.style.height = `${(sizeStep * pointDepth) + 2}px`;
+}
+initbg(3);
 
 const effect_red = document.querySelector("button[target='eff_red'");
 effect_red.addEventListener('click', (event) => {
@@ -100,5 +185,34 @@ function capture() {
     a.hidden = true;
     document.body.appendChild(a);
     a.innerHTML = "beep boop downloading";
-    a.click();
+
+    writingText.innerHTML = "";
+    writingText.style.animation = "incant 5s 1 ease-out";
+
+    paperSound_crush.rate(1.5);
+    paperSound_crush.play();
+    
+    let laugh1 = laughSound.play();
+    let laugh2 = laughSound.play();
+    let laugh3 = laughSound.play();
+    laughSound.rate(.5, laugh1);
+    laughSound.rate(.9, laugh2);
+    laughSound.rate(1.1, laugh3);
+
+    setTimeout(function(){    
+        fireSound.fade(0, 1, 500);
+        fireSound.play();
+        setTimeout(function(){
+            fireSound.fade(1, 0, 500);
+        }, 2000);
+    }, 2000);
+
+    setTimeout(function(){
+        paperSound_reveal.play();
+    }, 4500);
+
+    setTimeout(function(){
+        writingText.style.animation = "";
+        a.click();
+    }, 5000);
 }
