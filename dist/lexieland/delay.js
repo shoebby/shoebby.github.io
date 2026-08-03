05d65bfe458348a409c9d78406ae3ffc19f69929
shoebby <alexandria.vaneekelen@gmail.com>
2026-08-04 00:36:39 +0200

    talking to strangers simulator

--- dist/lexieland/delay.js
+++ dist/lexieland/delay.js
@@ -1,3 +1,9 @@
 function delay (URL) {
+
+    handimg.src = "../images/lexieland/hand-snap.gif";
+    handimg.style.transform = "scale(1.5)";
+    bg.stop();
+    snap.play();
+    
     setTimeout( function() { window.location = URL }, 1000 );
 }
--- dist/lexieland/index.js
+++ dist/lexieland/index.js
@@ -1,57 +1,84 @@
 const hand = document.querySelector("#hand");
 const handimg = hand.querySelector("img");
 
-const anchors = document.querySelectorAll("a");
+const actors = document.querySelectorAll(".interactable a");
 
 const itemToScroll = document.querySelector("html");
 
+const talker = document.querySelector("#talker");
+let dia_name = dialogue.querySelector("#name");
+let dia_text = dialogue.querySelector("#text");
+let dia_link = dialogue.querySelector("#link");
+let dia_cancel = dialogue.querySelector("#cancel");
+
+let currentActor = null;
+let currentLink = "";
+
 const bg = new Howl({
-  src: ['../sounds/lexieland/bg.mp3'],
-  loop: [true],
-  autoplay: [true],
-  volume: [.3]
+	src: ['../sounds/lexieland/bg.mp3'],
+	loop: [true],
+	autoplay: [true],
+	volume: [.3]
 });
 const snap = new Howl({
-  src: ['../sounds/lexieland/snap.mp3'],
-  volume: [1]
+	src: ['../sounds/lexieland/snap.mp3'],
+	volume: [1]
 });
 const grab = new Howl({
-  src: ['../sounds/lexieland/grab.wav'],
-  volume: [.2]
+	src: ['../sounds/lexieland/grab.wav'],
+	volume: [.2]
 });
 const leadup = new Howl({
-  src: ['../sounds/lexieland/grab-leadup.wav'],
-  volume: [.5]
+  	src: ['../sounds/lexieland/grab-leadup.wav'],
+  	volume: [.5]
 });
 
 document.addEventListener("wheel", function(e) {
-  if (Math.abs(e.deltaY) > 0) {
-    e.preventDefault();
-    itemToScroll.scrollLeft += e.deltaY;
-  }
+  	if (Math.abs(e.deltaY) > 0) {
+    	e.preventDefault();
+    	itemToScroll.scrollLeft += e.deltaY;
+  	}
 });
 
-anchors.forEach(element => {
+dia_link.addEventListener("click", () => {
+	let outlink = document.createElement("a");
+	outlink.href = currentLink;
+
+	handimg.src = "../images/lexieland/hand-snap.gif";
+    handimg.style.transform = "scale(1.5)";
+    bg.stop();
+    snap.play();
+    
+    setTimeout( function() { outlink.click(); }, 1000 );
+})
+
+dia_cancel.addEventListener("click", () => {
+	talker.style.display = "none";
+	currentActor.removeAttribute("style");
+	currentActor = null;
+
+	actors.forEach(element => {
+		element.style.pointerEvents = "all";
+	})
+})
+
+actors.forEach(element => {
     element.addEventListener("click", () => {
-        element.style.pointerEvents = "none";
+        actors.forEach(element => {
+			element.style.pointerEvents = "none";
+		})
+		currentActor = element;
+		currentActor.style.visibility = "hidden";
 
-        const grabber = element.querySelector(".grabber");
-        const char = element.querySelector(".char");
-
-        grabber.style.animation = "grabber .5s linear 1";
-        grabber.src = "../images/lexieland/hand-grab-f1.webp";
-        handimg.src = "../images/lexieland/hand-snap.gif";
-        bg.stop();
-        leadup.play();
-        snap.play();
-
-        setTimeout(() => {
-            char.style.display = "none";
-            grabber.style.zIndex = 0;
-            grabber.style.marginLeft = "-15vw";
-            grabber.src = "../images/lexieland/hand-grab-f2.webp";
-            grab.play();
-        }, 500);
+		if (element.id == "jack") {
+			dia_name.innerHTML = "Jack the Bipper";
+			dia_text.innerHTML = "Hey bozo, I got this shed full of cool websites 'n webpages. Wanna check it out?";
+			dia_link.innerHTML = "Let's follow this sassy child.";
+			currentLink = "../coolwebsites.html";
+			dia_cancel.innerHTML = "Sketchy ass fucker. Hell no."
+			
+		}
+		talker.style.display = "block";
     });
 });
 
@@ -59,8 +86,12 @@ window.addEventListener( "pageshow", function ( event ) {
     let historyTraversal = event.persisted || ( typeof window.performance != "undefined" && window.performance.navigation.type === 2 );
 
     if ( historyTraversal ) {
-        // Handle page restore.
-        
+        // page restore.
         window.location.reload(true);
     }
 });
+
+export function delay (URL) {
+
+    
+}
