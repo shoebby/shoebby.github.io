9f8e3b99a4ae0a262ceb5e1218d82cb6a232476e
shoebby <alexandria.vaneekelen@gmail.com>
2026-08-14 00:24:45 +0200

    bio secitonm aaddd

--- dist/lexieland/delay.js
+++
@@ -1,9 +0,0 @@
-function delay (URL) {
-
-    handimg.src = "../images/lexieland/hand-snap.gif";
-    handimg.style.transform = "scale(1.5)";
-    bg.stop();
-    snap.play();
-    
-    setTimeout( function() { window.location = URL }, 1000 );
-}
